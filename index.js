0f6ac48b2dce1aca1daba0a57848a01ac8e8ed5e
pixeliumjp <sato@pixelium.jp>
2022-12-09 19:25:29 +0900

    修正

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-import Joi from 'joi'
+import Joi from 'joi';
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
 import * as capture from './capture.js';
 
 const app = express();
-const dirname = path.dirname(new URL(import.meta.url).pathname)
+const dirname = path.dirname(new URL(import.meta.url).pathname);
 
-app.use(express.static(path.join(dirname, 'public')))
+app.use(express.static(path.join(dirname, 'public')));
 
 app.get('/', (req, res) => {
-	res.json({ "pet": "dog" });
-	console.log('GETリクエストを受け取りました')
+	res.json({ pet: 'dog' });
+	console.log('GETリクエストを受け取りました');
 	res.end();
-})
+});
 
 app.get('/api/capture', function (req, res) {
-
 	const schema = Joi.object({
 		width: Joi.number(),
 		height: Joi.number(),
 		url: Joi.string().uri(),
 		wait: Joi.number(),
 		full: Joi.bool(),
-	})
+	});
 
 	const result = schema.validate(req.query);
 
@@ -32,17 +31,22 @@ app.get('/api/capture', function (req, res) {
 		return;
 	}
 
-	test(result).then(response => {
+	test(result).then((response) => {
 		fs.readFile('./dest/screenshot.png', (err, data) => {
 			res.type('png');
 			res.send(data);
 		});
-	})
-
+	});
 });
 
 async function test(result) {
-	await capture.captureScreenshot(result.value.url, result.value.width, result.value.height, result.value.wait, result.value.full);
+	await capture.captureScreenshot(
+		result.value.url,
+		result.value.width,
+		result.value.height,
+		result.value.wait,
+		result.value.full
+	);
 	await capture.imageOptimization();
 }
 
